4cb766d4185a00fe4fb84023f3e11c41255463a7
Miron Melnikov <melnikovmiron33@gmail.com>
2022-02-20 15:00:12 +0300

    Добавьте файлы проекта.

---
+++ home-task1/Program.cs
@@ -0,0 +1,14 @@
+﻿while (true)
+{
+    Console.WriteLine("Введите длину стороны прямоуголника - а");
+    float a = float.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите длину стороны прямоуголника - b");
+    float b = float.Parse(Console.ReadLine());
+
+    float p = (a + b) * 2;
+    Console.WriteLine($"Периметр прямоуголника равен: {p}");
+
+    float s = a * b;
+    Console.WriteLine($"Площадь прямоуголника равна: {s}");
+}
