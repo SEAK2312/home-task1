8b98fb32493525ca0c7b890ab4c20ebaeb127622
Miron Melnikov <melnikovmiron33@gmail.com>
2022-02-20 15:25:15 +0300

    Добвлен цикл

--- home-task1/Program.cs
+++ home-task1/Program.cs
@@ -1,13 +1,16 @@
-﻿Console.WriteLine("Привет! Это ветка Мирона");
-Console.WriteLine("Введите длину стороны прямоуголника - а");
-float a = float.Parse(Console.ReadLine());
+﻿while (true)
+{
+    Console.WriteLine("Привет! Это ветка Мирона");
+    Console.WriteLine("Введите длину стороны прямоуголника - а");
+    float a = float.Parse(Console.ReadLine());
 
-Console.WriteLine("Введите длину стороны прямоуголника - b");
-float b = float.Parse(Console.ReadLine());
+    Console.WriteLine("Введите длину стороны прямоуголника - b");
+    float b = float.Parse(Console.ReadLine());
 
-float p = (a + b) * 2;
-Console.WriteLine($"Периметр прямоуголника равен: {p}");
+    float p = (a + b) * 2;
+    Console.WriteLine($"Периметр прямоуголника равен: {p}");
 
-float s = a * b;
-Console.WriteLine($"Площадь прямоуголника равна: {s}");
-Console.WriteLine("Все работает! Я молодец!");
+    float s = a * b;
+    Console.WriteLine($"Площадь прямоуголника равна: {s}");
+    Console.WriteLine("Все работает! Я молодец!");
+}
