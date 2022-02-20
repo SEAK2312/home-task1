b26d4f2904406f443ee091b80e0e5d441b9e178a
Miron Melnikov <melnikovmiron33@gmail.com>
2022-02-20 15:37:34 +0300

    Добавлено условие

--- home-task1/Program.cs
+++ home-task1/Program.cs
@@ -1,16 +1,22 @@
 ﻿while (true)
 {
-    Console.WriteLine("Привет! Это ветка Мирона");
+    Console.WriteLine("Что рассчитать, периметр или площадь (p или s):");
+    string formula = Console.ReadLine();
+
     Console.WriteLine("Введите длину стороны прямоуголника - а");
     float a = float.Parse(Console.ReadLine());
 
     Console.WriteLine("Введите длину стороны прямоуголника - b");
     float b = float.Parse(Console.ReadLine());
 
-    float p = (a + b) * 2;
-    Console.WriteLine($"Периметр прямоуголника равен: {p}");
+    if (formula == "p")
+    {
+        float p = (a + b) * 2;
+        Console.WriteLine($"Периметр прямоуголника равен: {p}");
+    }
 
     float s = a * b;
     Console.WriteLine($"Площадь прямоуголника равна: {s}");
+
     Console.WriteLine("Все работает! Я молодец!");
 }
