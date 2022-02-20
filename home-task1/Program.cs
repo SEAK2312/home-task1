8f7a9df0f14f11db19dc4cc4bed3e8fa1b8cf668
Miron Melnikov <melnikovmiron33@gmail.com>
2022-02-20 15:08:15 +0300

    develop branch created by Sergey

--- home-task1/Program.cs
+++ home-task1/Program.cs
@@ -1,14 +1,13 @@
-﻿while (true)
-{
-    Console.WriteLine("Введите длину стороны прямоуголника - а");
-    float a = float.Parse(Console.ReadLine());
+﻿Console.WriteLine("Привет! Это ветка Сергея");
+Console.WriteLine("Введите длину стороны прямоуголника - а");
+float a = float.Parse(Console.ReadLine());
 
-    Console.WriteLine("Введите длину стороны прямоуголника - b");
-    float b = float.Parse(Console.ReadLine());
+Console.WriteLine("Введите длину стороны прямоуголника - b");
+float b = float.Parse(Console.ReadLine());
 
-    float p = (a + b) * 2;
-    Console.WriteLine($"Периметр прямоуголника равен: {p}");
+float p = (a + b) * 2;
+Console.WriteLine($"Периметр прямоуголника равен: {p}");
 
-    float s = a * b;
-    Console.WriteLine($"Площадь прямоуголника равна: {s}");
-}
+float s = a * b;
+Console.WriteLine($"Площадь прямоуголника равна: {s}");
+Console.WriteLine("Готово");
